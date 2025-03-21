d8c9f5fa3c89a4c8c93c38745232a7bdfe1f0356
cbrady2718 <cbrady2718@gmail.com>
2025-03-20 17:18:32 -0700

    render

--- letterboxed.py
+++ letterboxed.py
@@ -3,6 +3,7 @@ import logging
 import re
 import time
 
+import os
 import requests
 from bs4 import BeautifulSoup
 from utils.UndirectedGraph import UndirectedGraph
@@ -130,5 +131,7 @@ def solve():
 
     return jsonify({'solution': solution})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
